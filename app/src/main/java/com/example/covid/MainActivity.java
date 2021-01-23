cb600bd99ffe8621dc99032e151604c0bb4f85b9
ParkerMorr <parker.morris@colorado.edu>
2021-01-23 13:52:56 -0500

    Buggy front end

--- app/src/main/java/com/example/covid/MainActivity.java
+++ app/src/main/java/com/example/covid/MainActivity.java
@@ -6,14 +6,23 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.covid.ui.main.SectionsPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +36,11 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
         FloatingActionButton fab = findViewById(R.id.fab);
+        RecyclerView rv = (RecyclerView)findViewById(R.id.rv);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        rv.setLayoutManager(llm);
+        RVAdapter adapter=new RVAdapter(Data);
+        rv.setAdapter(adapter);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,5 +49,55 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+    class InsertData{
+        String state;
+        int cases;
+        InsertData(String state,int cases){
+            this.state=state;
+            this.cases=cases;
+        }
+    }
+    private List<InsertData> Data;
+    private void Init(){
+        Data=new ArrayList<>();
+        Data.add(new InsertData("Florida",4000));
+        Data.add(new InsertData("Colorado",2000));
+
+    }
+    public class RVAdapter extends RecyclerView.Adapter<RVAdapter.DataViewHolder>{
+        List <InsertData> Data;
+        public class DataViewHolder extends RecyclerView.ViewHolder{
+            CardView card_view;
+            TextView Cases;
+            DataViewHolder(View itemView){
+                super(itemView);
+                card_view=(CardView)itemView.findViewById(R.id.card_view);
+                Cases=(TextView)itemView.findViewById(R.id.Cases);
+            }
+        }
+
+        RVAdapter(List<InsertData> Data){
+            this.Data=Data;
+        }
+        @Override
+        public int getItemCount(){
+            return Data.size();
+        }
+
+        @Override
+        public DataViewHolder onCreateViewHolder(ViewGroup viewGroup, int i){
+            View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.fragment_main,viewGroup,false);
+            DataViewHolder dvh = new DataViewHolder(v);
+            return dvh;
+        }
+        @Override
+        public void onBindViewHolder(DataViewHolder dataViewHolder, int i){
+            dataViewHolder.Cases.setText(Data.get(i).cases);
+        }
+        @Override
+        public void onAttachedToRecyclerView(RecyclerView recyclerView){
+            super.onAttachedToRecyclerView(recyclerView);
+        }
     }
 }
